c105f30c90433be1c357c9537844847b493cb40e
Dave Gamble <davegamble@gmail.com>
2024-06-16 12:52:56 +0100

    This is a bit fiddly

--- Map.cpp
+++ Map.cpp
@@ -61,18 +61,12 @@ void Map::RenderBSPNodes(int iNodeID)
 		if (V1ToV2Span >= 180) continue;
 		Angle V1AngleFromPlayer = V1Angle - m_Angle; // Rotate every thing.
 		Angle V2AngleFromPlayer = V2Angle - m_Angle;
-		Angle V1Moved = V1AngleFromPlayer + m_HalfFOV; // Validate and Clip V1. shift angles to be between 0 and 90 (now virtualy we shifted FOV to be in that range)
-
-		if (V1Moved > m_FOV)
+		if (V1AngleFromPlayer + m_HalfFOV > m_FOV)
 		{
-			if (V1Moved - m_FOV >= V1ToV2Span) continue; // now we know that V1, is outside the left side of the FOV But we need to check is Also V2 is outside. Lets find out what is the size of the angle outside the FOV // Are both V1 and V2 outside?
+			if (V1AngleFromPlayer - m_HalfFOV >= V1ToV2Span) continue; // now we know that V1, is outside the left side of the FOV But we need to check is Also V2 is outside. Lets find out what is the size of the angle outside the FOV // Are both V1 and V2 outside?
 			V1AngleFromPlayer = m_HalfFOV; // At this point V2 or part of the line should be in the FOV. We need to clip the V1
 		}
 		if (m_HalfFOV - V2AngleFromPlayer > m_FOV) V2AngleFromPlayer = -m_HalfFOV; // Validate and Clip V2 // Is V2 outside the FOV?
-
-		V1AngleFromPlayer += 90;
-		V2AngleFromPlayer += 90;
-		
-		m_pViewRenderer->AddWallInFOV(seg, V1Angle, V2Angle, V1AngleFromPlayer, V2AngleFromPlayer);
+		m_pViewRenderer->AddWallInFOV(seg, V1Angle, V2Angle, V1AngleFromPlayer + 90, V2AngleFromPlayer + 90);
 	}
 }

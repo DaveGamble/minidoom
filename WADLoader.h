dfacd52b8fa2642af6ce62016492803f5476b0c0
Dave Gamble <davegamble@gmail.com>
2024-06-16 01:39:43 +0100

    and making this public opens up the possibility of flattening the map build

--- WADLoader.h
+++ WADLoader.h
@@ -14,14 +14,13 @@ public:
 		int id = FindLumpByName(name, after);
 		return (id == -1) ? std::vector<uint8_t>() : std::vector<uint8_t>(data + dirs[id].LumpOffset, data + dirs[id].LumpOffset + dirs[id].LumpSize);
 	}
-
-protected:
-    int FindLumpByName(const std::string &LumpName, size_t after = 0) const
+	int FindLumpByName(const std::string &LumpName, size_t after = 0) const
 	{
 		for (size_t i = after; i < numLumps; ++i) if (!strncasecmp(dirs[i].LumpName, LumpName.c_str(), 8)) return (int)i;
 		return -1;
 	}
 
+protected:
 	const Directory* dirs {nullptr};
 	size_t numLumps {0};
 	uint8_t *data {nullptr};

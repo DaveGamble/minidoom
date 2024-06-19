8a4dd6abcd22231498f06d00ee81af2f9cb8758d
Dave Gamble <davegamble@gmail.com>
2024-06-19 02:17:53 +0100

    ouch

--- ViewRenderer.cpp
+++ ViewRenderer.cpp
@@ -63,8 +63,10 @@ void ViewRenderer::addWallInFOV(const Seg &seg, const Viewpoint &v)
 		//
 		// case 1: tov1z + tov1x + t * (tov2z - tov1z + tov2x - tov1x) = 0, so t = -(tov1z + tov1x) / (tov2z - tov1z + tov2x - tov1x)
 		// case 2: tov1z - tov1x + t * (tov2z - tov1z - tov2x + tov1x) = 0, so t = (tov1z - tov1x) / (tov2z - tov1z - tov2x + tov1x)
-		double t1 = -(tov1z + tov1x) / (tov2z - tov1z + tov2x - tov1x), t2 = (-tov1z + tov1x) / (tov2z - tov1z - tov2x + tov1x);
-		if ((t1 < 0 || t1 > 1 || (tov1z + t1 * (tov2z - tov1z) < 0)) && (t2 < 0 || t2 > 1 || (tov1z + t2 * (tov2z - tov1z) < 0))) return;
+		float t1n = -tov1z - tov1x, t1d = tov2z - tov1z + tov2x - tov1x, t2n = -tov1z + tov1x, t2d = tov2z - tov1z - tov2x + tov1x;
+		if (t1d < 0) {t1n = -t1n; t1d = -t1d;}
+		if (t2d < 0) {t2n = -t2n; t2d = -t2d;}
+		if ((t1n < 0 || t1n > t1d || (tov1z * t1d + t1n * (tov2z - tov1z) < 0)) && (t2n < 0 || t2n > t2d || (tov1z * t2d + t2n * (tov2z - tov1z) < 0))) return;
 
 //		if (amod(atan2f(-tov1x, tov1z) - M_PI_4) >= amod(atan2f(-tov1x, tov1z) - atan2f(-tov2x, tov2z))) return; // now we know that V1, is outside the left side of the FOV But we need to check is Also V2 is outside. Lets find out what is the size of the angle outside the FOV // Are both V1 and V2 outside?
 //		if (atan2f(tov1y - tov1x, tov1x + tov1y) + 2 * M_PI  + atan2f(tov2y, tov2x) >= atan2f(tov1y, tov1x)) return; // now we know that V1, is outside the left side of the FOV But we need to check is Also V2 is outside. Lets find out what is the size of the angle outside the FOV // Are both V1 and V2 outside?

cb2579d88d00daa5a8eb7de805147194b2996862
Dave Gamble <davegamble@gmail.com>
2024-06-25 01:00:19 +0100

    fix

--- Flat.hpp
+++ Flat.hpp
@@ -9,7 +9,7 @@ public:
 	uint8_t pixel(int u, int v) const { return data[(64 * v + (u & 63)) & 4095]; }
 	void renderSpan(uint8_t *buffer, int len, float u1, float v1, float du, float dv, const uint8_t *lut) const
 	{
-		for (int i = 0; i < len; i++) buffer[i] = lut[data[(int)(64*floor(64*(v1 + i * dv)) + floor(64*(u1 + i * du))) & 4095]];
+		for (int i = 0; i < len; i++, u1 += du, v1 += dv) buffer[i] = lut[data[((((int)v1) << 6) + (((int)u1) & 63)) & 4095]];
 	}
 protected:
 	uint8_t data[4096];
